1f3d8244be78822c3a816ddef2a52d996e1c9de3
scanf-ak27 <kraayush27@gmail.com>
2022-05-16 12:43:17 +0530

    first commit

---
+++ Calculator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+void main()
+{
+    int ch;
+    printf("------Calculator Options------\n\n");
+    printf("1. Addition\n2. Subtraction");
+
+}
+
